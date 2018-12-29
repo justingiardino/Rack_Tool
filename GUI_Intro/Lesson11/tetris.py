eb32bb9af980789998fb48185f49415268e2c62d
justingiardino13-Mac <justingiardino13@gmail.com>
2018-12-28 20:35:49 -0800

    Tetris basic painting and key press
    Pick up at timerEvent
    
    Mac

--- GUI_Intro/Lesson11/tetris.py
+++ GUI_Intro/Lesson11/tetris.py
@@ -82,7 +82,127 @@ class Board(QFrame):
         self.isPaused = False
         self.clearBoard()
 
-    #continue with shapeAt
+    #determine shape at board position
+    def shapeAt(self, x, y):
+        return self.board[(y * Board.BoardWidth) + x]
+
+    #assign shape at board position
+    def setShapeAt(self, x, y, shape):
+        self.board[(y * Board.BoardWidth) + x] = shape
+
+    #board can be dynamically changed in size
+    #determine width of one square
+    def squareWidth(self):
+        return self.contentsRect().width()
+
+    #determine height
+    def squareHeight(self):
+        return self.contentsRect().height()
+
+    #start game
+    def start(self):
+
+        if self.isPaused:
+            return
+
+        self.isStarted = True
+        self.isWaitingAfterLine = False
+        self.numLinesRemoved = 0
+        self.clearBoard()
+
+        #emit custom signal
+        self.msg2Statusbar.emit(str(self.numLinesRemoved))
+
+        self.newPiece()
+        self.timer.start(Board.speed, self)
+
+    #pause/unpause game
+    def pause(self):
+
+        if not self.isStarted:
+            return
+
+        #invert pause
+        self.isPaused = not self.isPaused
+
+        #pause
+        if self.isPaused:
+            self.timer.stop()
+            #emit custom signal
+            self.msg2Statusbar.emit('Pause')
+
+        #unpause
+        else:
+            self.timer.start(Board.Speed, self)
+            self.msg2Statusbar.emit(str(self.numLinesRemoved))
+
+        self.update()
+
+    #paint all shapes in game
+    def paintEvent(self, event):
+
+        painter = QPainter(self)
+        rect = self.contentsRect()
+
+        boardTop = rect.bottom() - Board.BoardHeight * self.squareHeight()
+
+        #2d array
+        for i in range(Board.BoardHeight):
+            for j in range(Board.BoardWidth):
+                shape = self.shapeAt(j, Board.BoardHeight - i - 1)
+
+                if shape != Tetrominoe.NoShape:
+                    self.drawSquare(painter, rect.left() + j * self.squareWidth(), boardTop + i * self.squareHeight(), shape)
+
+        if self.curPiece.shape() != Tetrominoe.NoShape:
+
+            for i in range(4):
+
+                x = self.curX + self.curPiece.x(i)
+                y = self.curY - self.curPiece.y(i)
+                self.drawSquare(painter, rect.left() + x * self.squareWidth(), boardTop + (Board.BoardHeight - y - 1) * self.squareHeight(), self.curPiece.shape())
+
+    def keyPressEvent(self, event):
+
+        if not self.isStarted or self.curPiece.shape() == Tetrominoe.NoShape:
+            super(Board, self).keyPressEvent(event)
+
+        key = event()
+
+        #press p to pause game
+        if key == Qt.Key_P:
+            self.pause()
+            return
+
+        #if the game already is paused do nothing on arrow push
+        if self.isPaused:
+            return
+
+        elif key == Qt.Key_Left:
+            #should use something like trymove for rack check
+            self.tryMove(self.curPiece, self.curX - 1, self.curY)
+
+        elif key == Qt.Key_Right:
+            self.tryMove(self.curPiece, self.curX + 1, self.curY)
+
+        #down arrow rotates the piece right
+        elif key == Qt.Key_Down:
+            self.tryMove(self.curPiece.rotateRight(), self.curX, self.curY)
+        #up arrow rotates the piece left
+        elif key == Qt.Key_Up:
+            self.tryMove(self.curPiece.rotateLeft(), self.curX, self.curY)
+
+        elif key == Qt.Key_Space:
+            self.dropDown()
+
+        elif key == Qt.Key_D:
+            self.oneLineDown()
+
+        else:
+            super(Board, self).keyPressEvent(event)
+
+    #left off on tiemr event 
+
 
 
 if __name__ == '__main__':
